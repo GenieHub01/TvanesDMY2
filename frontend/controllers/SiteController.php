eb4092c590e8794e0d8820fcaa8bee3994df7a11
Alexeis <tanks.eu@gmail.com>
2019-10-29 22:34:29 +0200

    Removed unused action

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -62,57 +62,6 @@ class SiteController extends BaseController
     }
 
 
-    public function actionWorldpay()
-    {
-
-
-        return $this->render('worldpay');
-    }
-
-    public function actionPay()
-    {
-        $request = Yii::$app->request;
-        $token = $request->post('token');
-        $worldpay = new Worldpay('T_S_a7b8747b-4627-4c18-a70b-7a69558e11ac');
-
-        $billing_address = array(
-            "address1"=>'123 House Road',
-            "address2"=> 'A village',
-            "address3"=> '',
-            "postalCode"=> 'EC1 1AA',
-            "city"=> 'London',
-            "state"=> '',
-            "countryCode"=> 'GB',
-        );
-
-        try {
-            $response = $worldpay->createOrder(array(
-                'token' => $token,
-                'amount' => 500,
-                'currencyCode' => 'GBP',
-                'name' => 'test name',
-                'billingAddress' => $billing_address,
-                'orderDescription' => 'Order description',
-                'customerOrderCode' => 'Order code'
-            ));
-            if ($response['paymentStatus'] === 'SUCCESS') {
-                $worldpayOrderCode = $response['orderCode'];
-            } else {
-                throw new WorldpayException(print_r($response, true));
-            }
-        } catch (WorldpayException $e) {
-            echo 'Error code: ' .$e->getCustomCode() .'
-    HTTP status code:' . $e->getHttpStatusCode() . '
-    Error description: ' . $e->getDescription()  . '
-    Error message: ' . $e->getMessage();
-        } catch (Exception $e) {
-            echo 'Error message: '. $e->getMessage();
-        }
-
-        //return $this->render('pay');
-    }
-
-
     public function actionComparsionImport()
     {
         set_time_limit(0);
